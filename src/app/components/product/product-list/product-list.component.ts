6abc37019091f5825c7e78d2d6b322d55f07046a
graypidgeon <whitekangur@wp.pl>
2021-09-12 23:07:36 +0200

    Dodanie listy produktow i zmockowana lista

--- src/app/components/product/product-list/product-list.component.ts
+++ src/app/components/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product';
 
 @Component({
   selector: 'app-product-list',
@@ -11,5 +12,17 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  products:Product[] = [
+    {name: "asd", description: "ddd"},
+    {name: "bobo", description: "ssz"},
+    {name: "koko", description: "www"},
+  ];
 
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
 }
