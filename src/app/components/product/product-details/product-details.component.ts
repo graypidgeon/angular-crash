dd59753b74b3074a587ba45f05518a59141cd0e6
graypidgeon <whitekangur@wp.pl>
2021-09-13 18:12:59 +0200

    Dodanie przycisku do kupowania produktu

--- src/app/components/product/product-details/product-details.component.ts
+++ src/app/components/product/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/model/product';
+import { CartService } from 'src/app/services/product-list/cart.service';
 import { ProductsService } from 'src/app/services/product-list/products.service';
 
 @Component({
@@ -12,7 +13,9 @@ export class ProductDetailsComponent implements OnInit {
 
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute, private productsService: ProductsService) {}
+  constructor(private route: ActivatedRoute,
+            private productsService: ProductsService,
+            private cartService: CartService) {}
 
   ngOnInit(): void {
     // First get the product id from the current route.
@@ -22,6 +25,11 @@ export class ProductDetailsComponent implements OnInit {
     // Find the product that correspond with the id provided in route.
     this.product = this.productsService.getProduct(productIdFromRoute);
     console.log ("id ", productIdFromRoute, " product " + this.product );
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
   }
 
 }
